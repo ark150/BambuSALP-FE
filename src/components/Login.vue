5548d8c0dede6c27a2a8a227769ee401c4922e16
BaezCrdrm <dev.baez_crdrm@outlook.com>
2019-04-09 20:22:48 -0500

    Agregar login

---
+++ src/components/Login.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+      <md-field>
+          <label>Usuario</label>
+          <md-input class="loginput" v-model="initial" type="email" placeholder="Usuario"></md-input>
+      </md-field>
+
+      <md-field>
+          <label>Usuario</label>
+          <md-input class="loginput" v-model="initial" type="password" placeholder="Contraseña"></md-input>
+      </md-field>
+
+      <md-button class="md-raised md-primary">Iniciar</md-button>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+.loginput {
+  width:80%;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { MdButton, MdContent, MdField, MdInput } from 'vue-material/dist/components'
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default.css'
+
+Vue.use(MdButton)
+Vue.use(MdContent)
+Vue.use(MdField)
+// Vue.use(VueMaterial)
 
 Vue.config.productionTip = false
 
